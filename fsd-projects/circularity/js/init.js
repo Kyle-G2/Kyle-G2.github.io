01c367196035238b4814b07f98fc43fe08ddd454
Kyle-G2 <Galinokyle@gmail.com>
2025-06-26 13:38:39 +0000

    at least yellow light

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -31,18 +31,10 @@ var init = function (window) {
          view.addChild(circle);
          circles.push(circle);
         }
-
-
-        // TODO 3 : Call the drawCircle() function
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-
-
         // TODO 7 : Use a loop to create multiple circles
-
+        for (var i = 0; i < 100; i++) {
+            drawCircle(i)
+        }
 
 
 
@@ -56,41 +48,16 @@ var init = function (window) {
         and check to see if it has drifted off the screen.         
         */
         function update() {
-            // TODO 4 : Update the position of each circle using physikz.updatePosition()
-            physikz.updatePosition(
-                circles[0]
-            )
-            physikz.updatePosition(
-                circles[1]
-            )
-            physikz.updatePosition(
-                circles[2]
-            )
-            physikz.updatePosition(
-                circles[3]
-            )
-            physikz.updatePosition(
-                circles[4]
-            )
-            // TODO 5 : Call game.checkCirclePosition() on your circles
-           game.checkCirclePosition(
-            circles[0]
-           )
-           game.checkCirclePosition(
-            circles[1]
-           )
-           game.checkCirclePosition(
-            circles[2]
-           )
-           game.checkCirclePosition(
-            circles[3]
-           )
-           game.checkCirclePosition(
-            circles[4]
-           )
             // TODO 8 / TODO 9 : Iterate over the array
+           for (var m = 0; m < circles.length; m++){
+            physikz.updatePosition(
+                circles[m]
+            );
+            game.checkCirclePosition(
+                circles[m]
+            )
+           }
            
-            
         }
     
         /* 
